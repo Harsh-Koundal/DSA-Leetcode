8be6ab14a3fb41e3ec8c6447be785e27f2c54fd9
Harsh Koundal <aharsh3039@gmail.com>
2026-07-02 02:02:45 -0400

    Time: 14 ms (77.67%), Space: 45.8 MB (38.28%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int boats = 0;
+        int left = 0;
+        int right = people.size()-1;
+
+        sort(people.begin(),people.end());
+
+        while(left<=right){
+            if(people[left]+people[right]<=limit){
+                left++;
+                right--;
+            }else{
+                right--;
+            }
+            boats++;
+        }
+        return boats;
+    }
+};

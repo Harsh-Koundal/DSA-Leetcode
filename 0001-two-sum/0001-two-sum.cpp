626136382a4d67d96b6590cde3af58db0c1e0416
Harsh Koundal <aharsh3039@gmail.com>
2026-07-03 18:52:18 +0530

    Time: 4 ms (51.73%), Space: 14.8 MB (54.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -6,9 +6,10 @@ public:
         for(int i=0;i<nums.size();i++){
             int diff = target - nums[i];
 
-            if(mp.find(diff)!=mp.end()){
+            if(mp.find(diff)!= mp.end()){
                 return {mp[diff],i};
             }
+
             mp[nums[i]] = i;
         }
         return {};

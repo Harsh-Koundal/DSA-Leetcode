45744823b45c966dc8e206429d0d1e138ecd4d2e
Harsh Koundal <aharsh3039@gmail.com>
2026-07-15 13:02:51 +0530

    Time: 0 ms (100%), Space: 8.6 MB (52.06%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.cpp
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        vector<int> mp ={
-            1,2,3,4,5,6,7,8,9,0
-        };
         string ans = "";
 
         for(char ch : s){

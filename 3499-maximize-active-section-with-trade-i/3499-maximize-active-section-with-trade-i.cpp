59f004d4a9ce73d5284b874dcdc5e01b98820337
Harsh Koundal <aharsh3039@gmail.com>
2026-07-21 18:21:54 +0530

    Time: 193 ms (6.3%), Space: 176 MB (5.4%) - LeetHub

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+      int totalOnes = 0;
+        for (char c : s)
+            if (c == '1') totalOnes++;
+
+        string t = "1" + s + "1";
+
+        vector<pair<char, int>> runs;
+
+        for (char c : t) {
+            if (runs.empty() || runs.back().first != c)
+                runs.push_back({c, 1});
+            else
+                runs.back().second++;
+        }
+
+        int ans = totalOnes;
+
+        // Check every internal run of 1's
+        for (int i = 1; i + 1 < runs.size(); i++) {
+            if (runs[i].first == '1' &&
+                runs[i - 1].first == '0' &&
+                runs[i + 1].first == '0') {
+
+                int leftZeros = runs[i - 1].second;
+                int rightZeros = runs[i + 1].second;
+
+                ans = max(ans, totalOnes + leftZeros + rightZeros);
+            }
+        }
+
+        return ans;   
+    }
+};

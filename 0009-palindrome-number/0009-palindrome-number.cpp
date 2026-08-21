3188604a3e5024e98b0189a710baef5f87266452
Harsh Koundal <aharsh3039@gmail.com>
2026-08-21 22:33:00 +0530

    Time: 5 ms (18.62%), Space: 10.9 MB (7.01%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         string n = to_string(x);
-       int left = 0;
-       int right = n.length()-1;
+        int left  = 0;
+        int right = n.length()-1;
 
-       while(left < right){
-        if(n[left] != n[right]) return false;
-        left++;
-        right--;
-       }
-       return true;
+        while(left<right){
+            if(n[left] != n[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 };

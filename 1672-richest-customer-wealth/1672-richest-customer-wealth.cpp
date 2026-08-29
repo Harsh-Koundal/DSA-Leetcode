ceff28a7a0d144c9257b2c69c568c98227cf4219
Harsh Koundal <aharsh3039@gmail.com>
2026-08-29 11:35:27 +0530

    Time: 0 ms (100%), Space: 11.3 MB (81.96%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
-        int maxWelth = INT_MIN;
+        int ans = 0;
 
-        for(auto account : accounts){
-        int currWelth = 0;
-            for(int i=0;i<account.size();i++){
-                currWelth += account[i];
+        for(int i=0;i<accounts.size();i++){
+            int total = 0;
+            for(int j=0;j<accounts[0].size();j++){
+                total += accounts[i][j];
+
+                ans = max(ans,total);
             }
-                maxWelth = max(currWelth,maxWelth);
         }
-        return maxWelth;
+        return ans;
     }
 };
